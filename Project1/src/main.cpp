4b5e3de4600b3d29a3e931018e94b432d0a00ca0
angelfishhhh <3470233696@qq.com>
2026-02-01 15:34:06 +0800

    if && while

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
-	std::cout << "Hello , C++" << std::endl;
+	for (int count = 1 , i = 0; i < 5; count++ , i++)
+	{
+		std::cout << "hello , c++" <<std::setw(15) << " " << std::setfill('*') 
+				  << std::setw(5) << count << std::endl << std::setfill(' ');
+	}
+	std::cout << std::setw(60) << std::setfill('*') << "*" << std::endl << std::setfill(' ');
+
+	int i = 0 , count = 1;
+	while (i < 5)
+	{
+		std::cout << "hello , c++" << std::setw(15) << " " << std::setfill('*')
+			<< std::setw(5) << count << std::endl << std::setfill(' ');
+		i++ , count++;
+	}
+
 	return 0;
 }
